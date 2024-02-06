5a73422c3edc28f38b779f5708ab4ee8bcdf6018
Mahir Mahota <mahirmahota@gmail.com>
2024-02-06 16:08:54 -0500

    Fixed minor issues

--- Common/Src/can_driver.c
+++ Common/Src/can_driver.c
@@ -21,12 +21,12 @@ HAL_StatusTypeDef send_message(CANFrame self, uint32_t rtr) {
         tx_header.DLC += 1;
     }
     
-    return HAL_CAN_AddTxMessage(&(self.hcan), &tx_header, self.data, &tx_mailbox);
+    return HAL_CAN_AddTxMessage(self.hcan, &tx_header, self.data, &tx_mailbox);
 }
 
 HAL_StatusTypeDef get_message(CANFrame* self, uint32_t fifo_number) {
     CAN_RxHeaderTypeDef rx_header;
-    HAL_StatusTypeDef ret = HAL_CAN_GetRxMessage(&(self -> hcan), fifo_number, &rx_header, self -> data);
+    HAL_StatusTypeDef ret = HAL_CAN_GetRxMessage(self -> hcan, fifo_number, &rx_header, self -> data);
     if (ret != HAL_OK) {return ret;}
 
     self -> id = rx_header.StdId;
@@ -47,14 +47,14 @@ uint32_t get_segment(CANFrame self, DataSegment segment) {
     return ret;
 }
 
-uint8_t set_segment(CANFrame self, DataSegment segment, uint32_t data) {
-    if (self.id != segment.id) {
+uint8_t set_segment(CANFrame* self, DataSegment segment, uint32_t data) {
+    if ((self -> id) != segment.id) {
         return 1;
     }
 
     uint8_t length = segment.end - segment.start;
     for(uint8_t i = 0; i <= length; i++) {
-        self.data[(segment.start - 1) + i] = ((data >> (8 * i)) & 0xFF);
+        self -> data[(segment.start - 1) + i] = ((data >> (8 * i)) & 0xFF);
     }
     
     return 0;
