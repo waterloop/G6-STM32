8e453410074a6bf85e7879bb8e859371b62ed865
XanderHayhoe <xanderhayhoe@gmail.com>
2024-02-06 20:08:10 -0500

    fixed numbering

--- Common/Inc/can_driver.h
+++ Common/Inc/can_driver.h
@@ -1,22 +1,15 @@
 #pragma once
+
 #include <stdint.h>
 
-#define MAX_BYTES 8
+#include <stddef.h>
+#include "config.h"
 
 typedef struct {
-    uint32_t id;
-    uint8_t start;
-    uint8_t end;
+    uint32_t can_id; // ID of the message 
+    uint8_t start; // starting byte 
+    uint8_t end;  // ending byte
+
+
 } DataSegment;
 
-typedef struct {
-    CAN_HandleTypeDef* hcan;
-    uint32_t id;
-    uint8_t data[MAX_BYTES];
-} CANFrame;
-
-CANFrame CANFrame_init(CAN_HandleTypeDef* handler, uint32_t id);
-HAL_StatusTypeDef send_message(CANFrame self, uint32_t rtr);
-HAL_StatusTypeDef get_message(CANFrame* self, uint32_t fifo_number);
-uint32_t get_segment(CANFrame self, DataSegment segment);
-uint8_t set_segment(CANFrame* self, DataSegment segment, uint32_t bytes);
