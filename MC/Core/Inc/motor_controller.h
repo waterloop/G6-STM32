53a4aebd02b3b670b0fb149337df928b081a85ae
MostafaH04 <mahussein04@gmail.com>
2024-02-11 19:54:39 -0500

    Updated motor controller header

--- MC/Core/Inc/motor_controller.h
+++ MC/Core/Inc/motor_controller.h
@@ -9,15 +9,17 @@
 #define MOTOR_CONTROLLER_H
 
 #include "DAC.h"
-#include "telemetry.h"
+// #include "can_driver.h"
 
-struct GPIO_Pin{
+typedef struct{
 	GPIO_Typedef* port;
 	uint16_t pin;
-};
+} GPIO_Pin_t ;
 
-struct MotorController {
+typedef struct {
 	CAN_HandleTypeDef* hcan;
+	// TODO: Replace the hcan with CANFrame
+	// CANFrame can_frame;
 
 	DAC_Type* throttle;
 
@@ -25,7 +27,23 @@ struct MotorController {
 	GPIO_Pin* rev_pin;
 
 	GPIO_Pin* break_sw;
-};
+} Motor_Controller_t;
 
+Motor_Controller_t MC_Init(
+	CAN_HandleTypeDef* hcan,
+	// TODO: Replace the hcan with the CANFrame
+	// CANFrame can_frame,
+	DAC_Type* throttle_dac,
+	GPIO_Pin* forward_pin,
+	GPIO_Pin* reverse_pin,
+	GPIO_Pin* break_sw_pin
+);
+
+void MC_stop(Motor_Controller_t* MC);
+void MC_drive(Motor_Controller_t* MC);
+void MC_set_throttle(Motor_Controller_t* MC, float throttle);
+void MC_get_telem(Motor_Controller_t* MC);
+
+static float get_voltage(float throttle);
 
 #endif /* MOTOR_CONTROLLER_H */
