207614d7b80564361275380e2641040a85c42f02
Quinn Ivison <quinn.i@waterloop.ca>
2024-02-13 21:57:23 -0500

    Created project files

---
+++ BMS/STM32F767ZITX/Core/Inc/temp_sensing.h
@@ -0,0 +1,13 @@
+/*
+ * temp_sensing.h
+ *
+ *  Created on: Feb 13, 2024
+ *      Author: quinn
+ */
+
+#ifndef INC_TEMP_SENSING_H_
+#define INC_TEMP_SENSING_H_
+
+
+
+#endif /* INC_TEMP_SENSING_H_ */
