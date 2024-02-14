efeb0b056b3785b5365c809992194f8aac405d6c
Mahir Mahota <mahirmahota@gmail.com>
2024-02-14 13:59:41 -0500

    Added error handler

--- Common/Inc/can_driver.h
+++ Common/Inc/can_driver.h
@@ -2,6 +2,7 @@
 #include <stdint.h>
 
 #define MAX_BYTES 8
+uint32_t tx_mailbox;
 
 typedef struct {
     uint32_t id;
@@ -17,7 +18,7 @@ typedef struct {
 } CAN_Frame_t;
 
 CAN_Frame_t CAN_frame_init(CAN_HandleTypeDef* handler, uint32_t id);
-HAL_StatusTypeDef send_message(CAN_Frame_t self);
-HAL_StatusTypeDef get_message(CAN_Frame_t* self, uint32_t fifo_number);
+void send_message(CAN_Frame_t self);
+void get_message(CAN_Frame_t* self, uint32_t fifo_number);
 uint32_t get_segment(CAN_Frame_t self, Data_Segment_t segment);
 uint8_t set_segment(CAN_Frame_t* self, Data_Segment_t segment, uint32_t bytes);
