c93df3e6b6d560f56a82870722717d07fddb17ea
TharunGaneshram <76886618+TharunGaneshram@users.noreply.github.com>
2024-03-07 22:48:42 -0500

    change enqueue function to update final average temperature values

--- BMS/STM32F767ZITX/Core/Inc/temp_sensing.h
+++ BMS/STM32F767ZITX/Core/Inc/temp_sensing.h
@@ -12,6 +12,6 @@
 static uint8_t rear = -1;
 //static uint32_t temp_data[NUM_SAMPLES][NUM_MUX];
 
-void enqueue(uint32_t mux_avg[NUM_MUX], uint32_t **temp_data);
-void measure_temp(uint32_t *adc_data, uint32_t **temp_data);
+void enqueue(uint32_t *average_mux_temp, uint32_t mux_avg[NUM_MUX], uint32_t **temp_data);
+void measure_temp(uint32_t *average_mux_temp, uint32_t *adc_data, uint32_t **temp_data);
 
