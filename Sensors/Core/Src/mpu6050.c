48eece450d3a6e211e00371b7a32029fee64c775
Mahir Mahota <mahirmahota@gmail.com>
2024-04-30 17:34:44 -0400

    Successful pre test build

--- Sensors/Core/Src/mpu6050.c
+++ Sensors/Core/Src/mpu6050.c
@@ -1,9 +1,11 @@
 #include "mpu6050.h"
 #include "i2c.h" // You may need to include this for hi2c1
 
-float MPU6050_Read_Accel(char axis, I2C_HandleTypeDef i2c){
+static I2C_HandleTypeDef i2c_handler;
+
+float MPU6050_Read_Accel(char axis){
 	uint8_t Rec_Data[6];
-	HAL_I2C_Mem_Read(&i2c, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 6, 1000);
+	HAL_I2C_Mem_Read(&i2c_handler, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 6, 1000);
 
 	switch(axis){
 	case 'x':
@@ -26,9 +28,9 @@ float MPU6050_Read_Accel(char axis, I2C_HandleTypeDef i2c){
 	}
 }
 
-float MPU6050_Read_Gyro(char axis, I2C_HandleTypeDef i2c){
+float MPU6050_Read_Gyro(char axis){
     uint8_t Rec_Data[6];
-    HAL_I2C_Mem_Read(&i2c, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, Rec_Data, 6, 1000);
+    HAL_I2C_Mem_Read(&i2c_handler, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, Rec_Data, 6, 1000);
 
 	switch(axis){
 	case 'x':
@@ -51,20 +53,21 @@ float MPU6050_Read_Gyro(char axis, I2C_HandleTypeDef i2c){
 
 
 
-void MPU6050_Init (I2C_HandleTypeDef i2c){
+void MPU6050_Init (I2C_HandleTypeDef hi2c){
     uint8_t check, data;
-    HAL_I2C_Mem_Read(&i2c, MPU6050_ADDR, WHO_AM_I_REG, 1, &check, 1, 1000);
+    i2c_handler = hi2c;
+    HAL_I2C_Mem_Read(&i2c_handler, MPU6050_ADDR, WHO_AM_I_REG, 1, &check, 1, 1000);
 
     if (check == 104){
         data = 0;
-        HAL_I2C_Mem_Write(&i2c, MPU6050_ADDR, PWR_MGMT_1_REG, 1, &data, 1, 1000);
+        HAL_I2C_Mem_Write(&i2c_handler, MPU6050_ADDR, PWR_MGMT_1_REG, 1, &data, 1, 1000);
         data = 0x07;
-        HAL_I2C_Mem_Write(&i2c, MPU6050_ADDR, SMPLRT_DIV_REG, 1, &data, 1, 1000);
+        HAL_I2C_Mem_Write(&i2c_handler, MPU6050_ADDR, SMPLRT_DIV_REG, 1, &data, 1, 1000);
 
         data = 0x00;
-        HAL_I2C_Mem_Write(&i2c, MPU6050_ADDR, ACCEL_CONFIG_REG, 1, &data, 1, 1000);
+        HAL_I2C_Mem_Write(&i2c_handler, MPU6050_ADDR, ACCEL_CONFIG_REG, 1, &data, 1, 1000);
 
         data = 0x00;
-        HAL_I2C_Mem_Write(&i2c, MPU6050_ADDR, GYRO_CONFIG_REG, 1, &data, 1, 1000);
+        HAL_I2C_Mem_Write(&i2c_handler, MPU6050_ADDR, GYRO_CONFIG_REG, 1, &data, 1, 1000);
     }
 }
