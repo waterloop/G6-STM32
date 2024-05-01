d15ce8d4dd2a04b03982e73bccf83382881dfad6
Mahir Mahota <mahirmahota@gmail.com>
2024-04-30 20:09:06 -0400

    Fixing variables and adding default cases

--- Sensors/Core/Src/mpu6050.c
+++ Sensors/Core/Src/mpu6050.c
@@ -8,23 +8,23 @@ float MPU6050_Read_Accel(char axis){
 	HAL_I2C_Mem_Read(&i2c_handler, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 6, 1000);
 
 	switch(axis){
-	case 'x':
-		int16_t Accel_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data[1]);
-		float Ax = Accel_X_RAW / 16384.0;
-		return Ax;
+		case 'x':
+			int16_t Accel_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data[1]);
+			float Ax = Accel_X_RAW / 16384.0;
+			return Ax;
 
-	case 'y':
-		int16_t Accel_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data[3]);
-		float Ay = Accel_Y_RAW / 16384.0;
-		return Ay;
+		case 'y':
+			int16_t Accel_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data[3]);
+			float Ay = Accel_Y_RAW / 16384.0;
+			return Ay;
 
+		case 'z': //most likely won't need
+			int16_t Accel_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data[5]);
+			float Az = Accel_Z_RAW / 16384.0;
+			return Az;
 
-	case 'z': //most likely won't need
-		int16_t Accel_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data[5]);
-		float Az = Accel_Z_RAW / 16384.0;
-		return Az;
-
-
+		default:
+			return 0xFF;
 	}
 }
 
@@ -33,21 +33,23 @@ float MPU6050_Read_Gyro(char axis){
     HAL_I2C_Mem_Read(&i2c_handler, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, Rec_Data, 6, 1000);
 
 	switch(axis){
-	case 'x':
-		int16_t Gyro_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data[1]);
-		float Gx = Gyro_X_RAW / 131.0;
-		return Gx;
+		case 'x':
+			int16_t Gyro_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data[1]);
+			float Gx = Gyro_X_RAW / 131.0;
+			return Gx;
 
-	case 'y':
-		int16_t Gyro_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data[3]);
-		float Gy = Gyro_X_RAW / 131.0;
-		return Gy;
+		case 'y':
+			int16_t Gyro_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data[3]);
+			float Gy = Gyro_Y_RAW / 131.0;
+			return Gy;
 
+		case 'z': //most likely won't need
+			int16_t Gyro_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data[5]);
+			float Gz = Gyro_Z_RAW / 131.0;
+			return Gz;\
 
-	case 'z': //most likely won't need
-		int16_t Gyro_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data[5]);
-		float Gz = Gyro_X_RAW / 131.0;
-		return Gz;
+		default:
+			return 0xFF;
 	}
 }
 
