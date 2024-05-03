93ed06316efecb87244d7449ceee1e3e14b7ba16
Mahir Mahota <mahirmahota@gmail.com>
2024-05-03 00:54:56 -0400

    Fixed gyroscope logic

--- Sensors/Core/Src/mpu6050.c
+++ Sensors/Core/Src/mpu6050.c
@@ -11,7 +11,7 @@ void MPU6050_Init (I2C_HandleTypeDef hi2c){
     data = 0b00011000; // sets accelerometer range to +/- 16g
     HAL_I2C_Mem_Write(&i2c_handler, MPU6050_ADDR, ACCEL_CONFIG_REG, 1, &data, 1, TIMEOUT);
 
-    data = 0b00011000; // sets gyroscope range to +/- 2000deg/s
+    data = 0b00000000; // sets gyroscope range to +/- 250deg/s
     HAL_I2C_Mem_Write(&i2c_handler, MPU6050_ADDR, GYRO_CONFIG_REG, 1, &data, 1, TIMEOUT);
 
     data = 0b00000111; // sets polling rate to 1kHZ
@@ -34,11 +34,11 @@ void MPU6050_Read_Gyro(int8_t* x_gyro, int8_t* y_gyro, int8_t* z_gyro){
 	HAL_I2C_Mem_Read(&i2c_handler, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, rec_data, 6, TIMEOUT);
 
 	int16_t gyro_x_raw = (int16_t)(rec_data[0] << 8 | rec_data[1]);
-	*x_gyro = (int16_t)(gyro_x_raw * LSB_TO_RAD_S) >> 8;
+	*x_gyro = (int16_t)(gyro_x_raw * LSB_TO_RAD_S); // the value never goes above 256
 
 	int16_t gyro_y_raw = (int16_t)(rec_data[2] << 8 | rec_data[3]);
-	*y_gyro = (int16_t)(gyro_y_raw * LSB_TO_RAD_S) >> 8;
+	*y_gyro = (int16_t)(gyro_y_raw * LSB_TO_RAD_S);
 
 	int16_t gyro_z_raw = (int16_t)(rec_data[4] << 8 | rec_data[5]);
-	*z_gyro = (int16_t)(gyro_z_raw * LSB_TO_RAD_S) >> 8;
+	*z_gyro = (int16_t)(gyro_z_raw * LSB_TO_RAD_S);
 }
