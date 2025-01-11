98e59e10745b82f9cfe4107fb2060c58a597d014
Mahir Mahota <mahirmahota@gmail.com>
2025-01-11 16:00:19 -0500

    fixing pressure

--- Sensors/Core/Inc/pressure.h
+++ Sensors/Core/Inc/pressure.h
@@ -10,7 +10,9 @@
 
 #include <stdint.h>
 #define MAX_ADC_VALUE 4095
+#define MAX_VOLTS 3.3
 
+uint8_t volts_to_psi (uint8_t volts);
 uint8_t poll_pressure_sensor(void);
 
 #endif /* INC_PRESSURE_H_ */
